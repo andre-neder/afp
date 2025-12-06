00a826c68aed203ba436819ab871eac75d3cc48a
André Neder <andre.neder@mail.de>
2025-12-06 15:47:57 +0100

    fix sidebar color

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -4,7 +4,7 @@ export default function SideBar() {
 
   return (
     <div class="flex flex-col size-full p-2 gap-2">
-      <div class="font-bold text-lg mb-2">Slides</div>
+      <div class="font-bold text-lg mb-2 font-black">Slides</div>
 
     </div>
   );
